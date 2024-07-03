d5bb3a75d1fe68577fb0039e21291b3c3d0278e0
Dev Oza <55236505+Devojha408@users.noreply.github.com>
2024-07-03 08:29:20 +0000

    aws api

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api', // Change this to your server's URL if deployed
+    baseURL: 'http://ec2-43-204-218-94.ap-south-1.compute.amazonaws.com:5000/api', // Change this to your server's URL if deployed
 });
 
 export default axiosInstance;
